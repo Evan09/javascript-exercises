87d5a14303540864c739804ca5006b5c4b362e59
Evan Delasota <evan.delasota@gmail.com>
2019-10-08 18:42:52 -0700

    Added solution to piglatin using regex

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,7 +1,25 @@
-function translate() {
-	// body...
-}
+function translate(string) {
+	return string
+		.split(" ")
+		.map(word => {
+			const index = firstVowelLoc(word);
+			const beginning = word.slice(0, index);
+			const ending = word.slice(index);
+			return `${ending}${beginning}ay`;
+		})
+		.join(" ");
 
+};
+
+function firstVowelLoc(string) {
+	const vowels = string.match(/[aeiou]/g);
+	if (vowels[0] == "u" && string[string.indexOf(vowels[0]) - 1] == "q") {
+		return string.indexOf(vowels[1]);
+
+	}
+	return string.indexOf(vowels[0]);
+
+};
 
 module.exports = {
 	translate
